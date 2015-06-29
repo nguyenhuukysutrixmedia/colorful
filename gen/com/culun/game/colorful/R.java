1c24abcc27644f99763eaf1d21f906a986b1d6ee
nguyenhuuky.sutrixmedia@gmail.com <matkhaune123456>
2015-06-30 00:49:44 +0700

    ignore

--- gen/com/culun/game/colorful/R.java
+++ gen/com/culun/game/colorful/R.java
@@ -11,23 +11,30 @@ public final class R {
     public static final class attr {
     }
     public static final class bool {
-        public static final int is_test=0x7f040000;
+        public static final int is_test=0x7f050000;
+    }
+    public static final class color {
+        public static final int main_color=0x7f040000;
     }
     public static final class dimen {
-        public static final int box_margin_box=0x7f050000;
-        public static final int box_margin_wall=0x7f050001;
+        public static final int box_margin_box=0x7f060001;
+        public static final int box_margin_wall=0x7f060002;
+        public static final int box_round=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int main_activity_gameboard_surface_view=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
     }
     public static final class style {
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
     }
 }
